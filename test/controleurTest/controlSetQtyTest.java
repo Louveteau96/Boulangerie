3cce80df2868467987b5348c523773c2b67fadbe
Louveteau96 <elijahpeyrat@gmail.com>
2023-05-05 13:24:08 +0200

    Debut
    Création du corps du projet

---
+++ test/controleurTest/controlSetQtyTest.java
@@ -0,0 +1,20 @@
+package controleurTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class controlSetQtyTest {
+
+	@BeforeEach
+	void setUp() {
+		return;
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
